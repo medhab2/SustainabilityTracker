3e6472fe23b34d32c56b605866d03a3134659bcd
Medha <medhab2@illinois.edu>
2020-04-25 22:43:55 -0400

    publicTransportation

--- app/src/main/java/com/example/sustainabilitytracker/Walk.java
+++ app/src/main/java/com/example/sustainabilitytracker/Walk.java
@@ -1,7 +1,24 @@
 package com.example.sustainabilitytracker;
 
 public class Walk extends Transportation {
+    private static final double averageTime = -1;
+    private static final double averageDistance = -1;
     public Walk(String setType, double setTime, double setDistance) {
         super(setType, setTime, setDistance);
+    }
+    public static double getAverageTime() {
+        return averageTime;
+    }
+    public static double getAverageDistance() {
+        return averageDistance;
+    }
+    public double getTime() {
+        return super.getTime();
+    }
+    public double getDistance() {
+        return super.getDistance();
+    }
+    public String getType() {
+        return super.getType();
     }
 }
