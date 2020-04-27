e9619408d2319d4f203c0775bb722943794710df
bittman2 <bittman2@illinois.edu>
2020-04-27 10:40:25 -0500

    1

--- app/src/main/java/com/example/sustainabilitytracker/Walk.java
+++ app/src/main/java/com/example/sustainabilitytracker/Walk.java
@@ -1,7 +1,39 @@
 package com.example.sustainabilitytracker;
 
+import androidx.annotation.Nullable;
+
 public class Walk extends Transportation {
     public Walk(String setType, double setTime, double setDistance) {
         super(setType, setTime, setDistance);
+    }
+
+    @Override
+    public String getType() {
+        return super.getType();
+    }
+
+    @Override
+    public boolean ifCar(String type) {
+        return super.ifCar(type);
+    }
+
+    @Override
+    public double getDistance() {
+        return super.getDistance();
+    }
+
+    @Override
+    public double getTotalDistance() {
+        return super.getTotalDistance();
+    }
+
+    @Override
+    public double getTime() {
+        return super.getTime();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
     }
 }
