36ad5e099e0aae0f9e3d12d2deb770f58be77ccf
bittman2 <bittman2@illinois.edu>
2020-05-03 14:21:01 -0500

    1

--- app/src/main/java/com/example/sustainabilitytracker/PublicTransportationActivity.java
+++ app/src/main/java/com/example/sustainabilitytracker/PublicTransportationActivity.java
@@ -3,6 +3,7 @@ package com.example.sustainabilitytracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class PublicTransportationActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class PublicTransportationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_transportation);
+        EditText text = findViewById(R.id.publicTransitTime);
+        String temp = text.getText().toString();
+        double time = Double.parseDouble(temp);
     }
 }
