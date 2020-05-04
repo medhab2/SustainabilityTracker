a440f979629127fb74f7b56a1ef98bb754ce4fb5
bittman2 <bittman2@illinois.edu>
2020-05-04 13:41:20 -0500

    added pop up

--- app/src/main/java/com/example/sustainabilitytracker/WalkActivity.java
+++ app/src/main/java/com/example/sustainabilitytracker/WalkActivity.java
@@ -1,9 +1,44 @@
 package com.example.sustainabilitytracker;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.view.Display;
 import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class WalkActivity extends AppCompatActivity {
     private Walk walk;
@@ -12,7 +47,20 @@ public class WalkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk);
+        Button enter = findViewById(R.id.enterWalk);
+        enter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                createWalk();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                builder.setMessage("Total Distance Traveled: " + walk.getTotalDistance());
+                builder.setOnDismissListener(unused -> {
+                    finish();
+                });
+                builder.create().show();
+            }
+        });
     }
+
     public void createWalk() {
         EditText editText = findViewById(R.id.walkTime);
         String t = editText.getText().toString();
